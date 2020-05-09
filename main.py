fe82d4d0f0d6ff6935f8618719d02d3054afb890
Tundrabots Student <tundrabots@gmail.com>
2020-05-08 20:47:52 -0400

    Initial add of files

---
+++ main.py
@@ -0,0 +1,92 @@
+import pygame as pg
+import random
+from settings import *
+from sprites import *
+
+class Game:
+    def __init__(self):
+        #initialize game window, ect
+        pg.init()
+        pg.mixer.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
+        self.clock = pg.time.Clock()
+        self.running = True
+
+    def new(self):
+        #starts a new game
+        self.all_sprites = pg.sprite.Group()
+        self.platforms = pg.sprite.Group()
+        self.player = Player(self)
+        self.all_sprites.add(self.player)
+        for plat in PLATFORM_LIST:
+            p = Platform(*plat)
+            self.all_sprites.add(p)
+            self.platforms.add(p)
+
+    def run(self):
+        #game loop
+        self.playing = True
+        while self.playing == True:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
+
+    def update(self):
+        #game loop - update
+        self.all_sprites.update()
+        #check if player is on a platform only if falling
+        if self.player.vel.y > 0:
+            collisions = pg.sprite.spritecollide(self.player, self.platforms, False)
+            if collisions:
+                self.player.pos.y = collisions[0].rect.top
+                self.player.vel.y = 0
+                self.player.rect.midbottom = self.player.pos
+        #if player reaches the top 1/4 of the screen
+        if self.player.rect.top <= HEIGHT / 2.5:
+            self.player.rect.y += abs(self.player.vel.y)
+            for plat in self.platforms:
+                plat.rect.y += abs(self.player.vel.y)
+                if plat.rect.top >= HEIGHT:
+                    plat.kill()
+        #spawn new platforms
+        while len(self.platforms) < 6:
+            width = random.randrange(50, 100)
+            p = Platform(random.randrange(0, WIDTH - width), random.randrange(-50, -25), width, 20)
+            self.platforms.add(p)
+            self.all_sprites.add(p)
+
+    def events(self):
+        #game loop - events
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                if self.playing == True:
+                    self.playing = False
+                self.running = False
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    self.player.jump()
+
+    def draw(self):
+        #game loop - draw
+        self.screen.fill(BLACK)
+        self.all_sprites.draw(self.screen)
+        pg.display.flip()
+
+    def show_start_screen(self):
+        #game start screen
+        pass
+
+    def show_go_screen(self):
+        #game over/continue
+        pass
+
+g = Game()
+g.show_start_screen()
+while g.running:
+    g.new()
+    g.run()
+    g.show_go_screen()
+
+pg.quit()
---
+++ settings.py
@@ -0,0 +1,23 @@
+
+# game options/settings
+TITLE = "Jumpy"
+WIDTH = 480
+HEIGHT = 600
+FPS = 60
+
+#platforms
+PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40), (WIDTH / 2 - 50, HEIGHT * 3 / 4, 100, 20),
+                (125, HEIGHT - 280, 100, 20), (350, 200, 90, 20), (WIDTH / 2, 10, 50, 20)]
+
+#player properties
+PLAYER_ACC = 0.5
+PLAYER_FRICTION = -0.12
+PLAYER_GRAV = 0.8
+
+# colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0,255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
